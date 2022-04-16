4c638fd08ab6a9f0acdb0ab55cbeb4d1ababa073
ajay v <ajayvijayakumar2018@gmail.com>
2022-04-16 17:33:50 -0400

    pages folder and landing page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 
+import LandingPage from './pages/LandingPage';
+
 function App() {
-  const [getMessage, setGetMessage] = useState({})
+  //const [getMessage, setGetMessage] = useState({})
 
   useEffect(()=>{
     axios.get('https://soundalike2.vercel.app//flask/hello').then(response => {
       console.log("SUCCESS", response)
-      setGetMessage(response)
+      //setGetMessage(response)
     }).catch(error => {
       console.log(error)
     })
 
   }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React + Flask Tutorial</p>
-        <div>{getMessage.status === 200 ? 
-          <h3>{getMessage.data.message}</h3>
-          :
-          <h3>LOADING</h3>}</div>
-      </header>
-    </div>
+    <LandingPage />
   );
 }
 
